b1b00e980a3aeea2f41214b1226195d4c7c3c2a1
alex-rns <alexrns3@gmail.com>
2017-11-29 17:38:11 +0200

    1-st page complete

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    //---------------------- index-1 -----------------------
+
     $('.default-class').on('click', function () {
         $(this).addClass('button-full-width')
     });
@@ -30,6 +32,81 @@ $(document).ready(function () {
     });
 
     $('.clone-this').on('click',function () {
-        $(this).clone().text('cloned').after('clone-this')
+        $(this).clone().append(' cloned').insertAfter($(this))
+
     });
+
+    $('.closest-element').on('click', function () {
+        let myParent = $(this).closest($(this).parent());
+        console.log(myParent)
+    });
+
+    $('.each-btn-text').on('click', function () {
+        let buttonTextArray = [];
+        $('button').each( function () {
+            buttonTextArray.push($(this).text());
+            console.log(buttonTextArray)
+        })
+    });
+
+    $('.find-me').on('click', function () {
+        let findButton = $('body').find($(this));
+        console.log(findButton)
+    });
+
+    $('.fade-in-text').on('click', function () {
+        $('.fade-in-me').fadeIn()
+    });
+
+    $('.fade-out-text').on('click', function () {
+        $('.fade-out-me').fadeOut()
+    });
+
+    $('.hide-text').on('click', function () {
+        $('.hide-me').hide()
+    });
+
+    $('.show-text').on('click', function () {
+       $('.show-me').show()
+    });
+
+    $('.data-about-me').on('click', function () {
+        let dataHeigth = $(this).height();
+        let dataWidth = $(this).width();
+        let dataposition = $(this).position();
+        let dataAttr = $(this).attr('class');
+        let dataParent = $(this).parent().prop('tagName');
+        let dataPrev = $(this).prev().text();
+        let dataNext = $(this).next().text();
+        let dataText = $(this).text();
+
+        console.log('Высота:' + dataHeigth + ',',
+                    'Ширина:' + dataWidth + ',',
+                    'Абсолютная позиция по х:' + dataposition.left + ',',
+                    'Абсолютная позиция по y:' + dataposition.top + ',',
+                    'Значение атрибута:' + dataAttr + ',',
+                    'Родительский элемент:' + dataParent + ',',
+                    'Сосед сверху:' + dataPrev + ',',
+                    'Сосед снизу:' + dataNext + ',',
+                    'Текст кнопки:' + dataText + ','
+        )
+    });
+
+
+    /*---------------------- index-2 -----------------------*/
+
+
+    $('.data-example').on('click', function () {
+        $(this).data('tra-ta-ta', 'one');
+        console.log($(this).data());
+
+        $(this).data('123', "true");
+        console.log($(this).data());
+
+
+    });
+
+
+
+
 });
